2a5e169c0e4006507a2f830dbea2b8107b262df0
melisajuma <Melisaakinyi95@gmail.com>
2019-08-06 13:32:09 +0300

    importing Mail

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
-from flask_sqlalchemy import SQLAlchemy>
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_mail import Mail
+from flask_mail import Mail>
 
 
 bootstrap = Bootstrap()
