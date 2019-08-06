32d070a4558eb3df3aad058baadd81a3908c4d34
melisajuma <Melisaakinyi95@gmail.com>
2019-08-06 13:33:02 +0300

    chnages to the init.py file

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_mail import Mail>
+from flask_mail import Mail
 
 
 bootstrap = Bootstrap()
