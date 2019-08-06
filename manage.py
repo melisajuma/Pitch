55904a4ac397a604e29f57b134a267d2479fe2a3
melisajuma <Melisaakinyi95@gmail.com>
2019-08-06 12:18:22 +0300

    deployment chnages

--- manage.py
+++ manage.py
@@ -3,10 +3,8 @@ from flask_script import Manager, Server
 from app.models import User_pitch, Pitch, Comments, PitchCategory
 from flask_migrate import Migrate, MigrateCommand
 
-# Creating app instance
-# app = create_app('development')
 app = create_app('production')
-#app = create_app('test')
+
 
 manager = Manager(app)
 manager.add_command('server', Server)
