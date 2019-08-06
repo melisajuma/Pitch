c30164c529974ff83eff4ccf89e54b93ab554c05
melisajuma <Melisaakinyi95@gmail.com>
2019-08-06 14:38:34 +0300

    deployment

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config:
     
 class ProdConfig(Config):
     
-    SQLALCHEMY_DATABASE_URI= os.environ.get("DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:mel123@localhost/pitch'
         
 class DevConfig(Config):
     
